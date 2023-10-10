0035940817d3b0f5b45e74d9232d72eafd4789ec
biswajeetbiswal <biswajeetbiswal22@gmail.com>
2023-10-11 01:44:08 +0530

    added models

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const employeeRoute = require("./routes/employee");
+const adminRoute = require("./routes/admin");
+
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/employee_management", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Use admin routes
+app.use("/api/admin", adminRoute);
+
+app.use("/", (req, res) => {
+  res.send("This api is working!!");
+});
+
+// Use employee routes with admin authentication middleware
+app.use("/api/employees", employeeRoute);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ models/Employee.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  designation: {
+    type: String,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Employee", employeeSchema);
